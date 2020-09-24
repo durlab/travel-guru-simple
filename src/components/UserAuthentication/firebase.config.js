0c2a201861a94c9be3661ba9da5b889c8a701cef
durlab <kadurlab@gmail.com>
2020-09-24 21:01:16 +0600

    so hard

---
+++ src/components/UserAuthentication/firebase.config.js
@@ -0,0 +1,10 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyAtDyhlOPuPk8kngUQY4j3kDA73Y1fuvlk",
+    authDomain: "travel-guru-simple-app.firebaseapp.com",
+    databaseURL: "https://travel-guru-simple-app.firebaseio.com",
+    projectId: "travel-guru-simple-app",
+    storageBucket: "travel-guru-simple-app.appspot.com",
+    messagingSenderId: "402620615018",
+    appId: "1:402620615018:web:11819dce0dbeb6cb757d13"
+  };
+  export  default firebaseConfig;
